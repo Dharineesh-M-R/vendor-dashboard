61b974f21f762fa8d6831feec5968db4b1d6f765
Dharineesh-M-R <dharineeshmagudeswaran@gmail.com>
2025-06-28 23:08:25 +0530

    1.1

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+// backend/index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.get('/', (req, res) => res.send("Vendor API running"));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
