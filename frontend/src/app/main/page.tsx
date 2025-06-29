502d63cf224a0cebfa194f06e64e87269a36aba0
Dharineesh-M-R <dharineeshmagudeswaran@gmail.com>
2025-06-29 13:04:56 +0530

    1....1

--- frontend/src/app/main/page.tsx
+++ frontend/src/app/main/page.tsx
@@ -1,7 +1,9 @@
 export default function mainpage(){
     return (
         <main id="bg" className="bg-white h-screen">
-            
+            <div id="notification area" className="text-black">
+                the request from the company displays here
+            </div>
         </main>
     )
 }
