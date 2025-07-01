d75da076a753b9c5782b50219c08da989842157d
Dharineesh-M-R <dharineeshmagudeswaran@gmail.com>
2025-07-01 08:35:57 +0530

    main dashboard

--- frontend/src/app/main/page.tsx
+++ frontend/src/app/main/page.tsx
@@ -1,9 +1,39 @@
-export default function mainpage(){
-    return (
-        <main id="bg" className="bg-white h-screen">
-            <div id="notification area" className="text-black">
-                the request from the company displays here
-            </div>
-        </main>
-    )
+import { LayoutDashboard, Bell, Users, Car, FileText, Table } from 'lucide-react';
+
+export default function MainPage() {
+  return (
+    <>
+      <div className="grid grid-cols-12 gap-x-2 gap-y-2 h-screen">
+        <aside className="col-span-2 border-r p-6 space-y-6 shadow-sm">
+          <div className="text-xl text-black font-bold font-sans">Vendor Panel</div>
+          <nav className="space-y-4">
+            <a href="#dashboard" className="flex gap-2 items-center text-gray-500 hover:text-black">
+              <LayoutDashboard size={18} />
+              <span>Dashboard</span>
+            </a>
+            <a href="#booking" className="flex gap-2 items-center text-gray-500 hover:text-black">
+              <Bell size={18} />
+              <span>Bookings</span>
+            </a>
+            <a href="#drivers" className="flex gap-2 items-center text-gray-500 hover:text-black">
+              <Users size={18} />
+              <span>Drivers</span>
+            </a>
+            <a href="#vehicles" className="flex gap-2 items-center text-gray-500 hover:text-black">
+              <Car size={18} />
+              <span>Vehicles</span>
+            </a>
+            <a href="#invoices" className="flex gap-2 items-center text-gray-500 hover:text-black">
+              <FileText size={18} />
+              <span>Invoices</span>
+            </a>
+            <a href="#customtables" className="flex gap-2 items-center text-gray-500 hover:text-black">
+              <Table size={18} />
+              <span>Custom Tables</span>
+            </a>
+          </nav>
+        </aside>
+      </div>
+    </>
+  );
 }
